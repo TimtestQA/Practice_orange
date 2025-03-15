2fae494e79f9ec4d18ef9641c1c4b2bfa96239ac
TIm <nomail@mak.ru>
2025-03-16 00:50:23 +0300

    CI fixes conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,14 @@
 from fixtures.ui_fixtures import *
+
+
+@pytest.fixture(scope="function",autouse=True)
+def driver(request):
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sand-box")
+    options.add_argument("-disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    driver= webdriver.Chrome(options=options)
+    request.cls.driver = driver
+    yield
+    driver.quit()
